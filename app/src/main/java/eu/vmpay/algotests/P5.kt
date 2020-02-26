ba41bfa48264ede9f6a4343017bf8b966efe3de8
Andrew <vmpay.eu@gmail.com>
2020-02-26 23:24:56 +0100

    OOP tasks impl

---
+++ app/src/main/java/eu/vmpay/algotests/P5.kt
@@ -0,0 +1,13 @@
+package eu.vmpay.algotests
+
+/**
+ * Challenge 1 - The bakery
+ *
+ * The Warsaw bakery called "Cukiernia - ACC" is famous of it's donuts. It has 2 different kinds of
+ * those (standard - 2zł, extra large - 3.5zł) and each can be served with a different topping
+ * (cranberry/blueberry/raspberry/strawberry jam, nutella - 1zł). The overall price of the donut equals
+ * to the price of donut itself + the price of topping (can be more than one). Due limited number of
+ * exclusive donuts the bakery can sell one donut at a time.
+ *
+ * Goal: implement the bakery, donut, topping, order abstractions reflecting the above case.
+ */
