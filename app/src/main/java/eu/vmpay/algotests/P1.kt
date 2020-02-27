274239caae66f123fd82738036bd82fe3bd59d99
jurgas <sebastianjurga7@gmail.com>
2020-02-27 19:24:12 +0100

    P1 done

---
+++ app/src/main/java/eu/vmpay/algotests/P1.kt
@@ -0,0 +1,35 @@
+package eu.vmpay.algotests
+
+object P1 {
+
+    /**
+     * Write a function that returns the sum of two numbers.
+     * For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3.
+     */
+    fun add(param1: Int, param2: Int): Int {
+        return param1 + param2
+    }
+
+    /**
+     * Given a year, return the century it is in. The first century spans from the year 1 up to and
+     * including the year 100, the second - from the year 101 up to and including the year 200, etc.
+     * For year = 1905, the output should be centuryFromYear(year) = 20;
+     * For year = 1700, the output should be centuryFromYear(year) = 17.
+     */
+    fun centuryFromYear(year: Int): Int {
+        return if (year.rem(100) == 0)
+            year / 100
+        else
+            year /100 + 1
+    }
+
+    /**
+     * Given the string, check if it is a palindrome.
+     * For inputString = "aabaa", the output should be checkPalindrome(inputString) = true;
+     * For inputString = "abac", the output should be checkPalindrome(inputString) = false;
+     * For inputString = "a", the output should be checkPalindrome(inputString) = true.
+     */
+    fun checkPalindrome(inputString: String): Boolean {
+        return inputString == inputString.reversed()
+    }
+}
