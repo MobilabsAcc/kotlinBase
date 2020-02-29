406c67d90566e8c3ffe46c59b05a8554b7b0f783
Eryk Dobaj <3rykd084j@gmail.com>
2020-02-29 12:52:42 +0100

    P3 DONE - v1

--- app/src/main/java/eu/vmpay/algotests/P3.kt
+++ app/src/main/java/eu/vmpay/algotests/P3.kt
@@ -8,7 +8,18 @@ object P3 {
      * allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
      */
     fun allLongestStrings(inputArray: MutableList<String>): MutableList<String> {
-        TODO("not implemented")
+        var longestString = inputArray[0].length
+        for (string in inputArray){
+            if(string.length > longestString)
+                longestString = string.length
+        }
+        var returnList = mutableListOf<String>()
+        for(string in inputArray){
+            if(string.length == longestString){
+                returnList.add(string)
+            }
+        }
+        return returnList
     }
 
     /**
@@ -16,8 +27,22 @@ object P3 {
      * For s1 = "aabcc" and s2 = "adcaa", the output should be commonCharacterCount(s1, s2) = 3.
      * Strings have 3 common characters - 2 "a"s and 1 "c".
      */
+    // TO DO - remove break
     fun commonCharacterCount(s1: String, s2: String): Int {
-        TODO("not implemented")
+        var ss1 = s1
+        var ss2 = s2
+        var sum = 0
+        for (i1 in ss1){
+            for(i2 in ss2){
+                if(i1 == i2){
+                    ss1 = ss1.replaceFirst(i1 + "", "")
+                    ss2 = ss2.replaceFirst(i2 + "", "")
+                    sum++
+                    break
+                }
+            }
+        }
+        return sum
     }
 
     /**
@@ -28,7 +53,25 @@ object P3 {
      * For n = 239017, the output should be isLucky(n) = false.
      */
     fun isLucky(n: Int): Boolean {
-        TODO("not implemented")
+        var numberOfDigits = 0
+        var copyNumber = n
+        while(copyNumber > 0){
+            copyNumber /= 10
+            numberOfDigits++
+        }
+        copyNumber = n
+        var counter = 0
+        var sumFirstHalf = 0
+        var sumSecondHalf = 0
+        while(copyNumber > 0){
+            when{
+                counter < numberOfDigits/2 -> sumSecondHalf += copyNumber % 10
+                else -> sumFirstHalf += copyNumber % 10
+            }
+            copyNumber /= 10
+            counter++
+        }
+        return sumFirstHalf == sumSecondHalf
     }
 
     /**
@@ -39,7 +82,26 @@ object P3 {
      * the output should be sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
      */
     fun sortByHeight(a: MutableList<Int>): MutableList<Int> {
-        TODO("not implemented")
+        var extraList = mutableListOf<Int>()
+        for (element in a){
+            if(element != -1)
+                extraList.add(element)
+        }
+        for (i in 0 until extraList.size)
+            for(j in 1 until  extraList.size){
+                if(extraList[j-1] > extraList[j]){
+                    var tmp = extraList[j-1]
+                    extraList[j-1] = extraList[j]
+                    extraList[j] = tmp
+                }
+            }
+        var counter = 0
+        for ((index, value) in a.withIndex()){
+            if(value != -1){
+                a[index] = extraList[counter++]
+            }
+        }
+        return a
     }
 
     /**
@@ -54,7 +116,44 @@ object P3 {
      * reverseInParentheses(inputString) = "foobazrabblim".
      * Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".
      */
+
+    // TO DO: CLEAN!!
     fun reverseInParentheses(inputString: String): String {
-        TODO("not implemented")
+
+        var st = mutableListOf<Int>()
+        var string = inputString
+        var returnstring = inputString
+        var dlugosc = string.length
+        var jj = -1
+
+        for(i in 0 until dlugosc){
+            if(returnstring[i] == '('){
+                st.add(i)
+                jj++
+            }else if(returnstring[i] == ')'){
+                returnstring = returnstring.replaceRange(
+                    st.last(),
+                    i,
+                    returnstring.substring(st.last(), i).reversed()
+                )
+                st.removeAt(jj)
+                jj--
+            }
+        }
+        var returnn = ""
+        for (i in 0 until returnstring.length){
+            if(returnstring[i] != ')' && returnstring[i] != '(')
+                returnn += returnstring[i]
+        }
+        return returnn
+
+    }
+
+    fun findIndexClosingBracket(inputString: String, startIndex: Int): Int{
+        for(index in startIndex until  inputString.length){
+            if(inputString[index] == ')')
+                return index
+        }
+        return 0
     }
 }
