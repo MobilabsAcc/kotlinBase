51068f84f25d2411565ef7420a8983cb99ac341b
Itanus <rob.cwi@hotmail.com>
2020-02-29 19:24:30 +0100

    zadanie 3

--- app/src/main/java/eu/vmpay/algotests/P3.kt
+++ app/src/main/java/eu/vmpay/algotests/P3.kt
@@ -8,6 +8,14 @@ object P3 {
      * allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
      */
     fun allLongestStrings(inputArray: MutableList<String>): MutableList<String> {
+        val maxSize = inputArray.maxBy { it.length }!!.length
+        val array = mutableListOf<String>()
+        for(item in inputArray){
+            if(item.length == maxSize){
+                array.add(item)
+            }
+        }
+        return array
         TODO("not implemented")
     }
 
@@ -17,6 +25,18 @@ object P3 {
      * Strings have 3 common characters - 2 "a"s and 1 "c".
      */
     fun commonCharacterCount(s1: String, s2: String): Int {
+        var sum = 0
+        var string2 = s2
+        for (char1 in s1){
+            for(char2 in string2){
+                if(char1 == char2){
+                    string2 = string2.replaceFirst(char2,' ')
+                    sum++
+                    break
+                }
+            }
+        }
+        return sum
         TODO("not implemented")
     }
 
@@ -28,6 +48,20 @@ object P3 {
      * For n = 239017, the output should be isLucky(n) = false.
      */
     fun isLucky(n: Int): Boolean {
+        val size = n.toString().length
+        var sum1 = 0
+        var sum2 = 0
+        var tmp = n
+        for(i in 0 until size/2){
+            sum1 += tmp%10
+            tmp /= 10
+        }
+        for(i in 0 until size/2){
+            sum2 += tmp%10
+            tmp /= 10
+        }
+        if(sum1 == sum2)return true
+        return false
         TODO("not implemented")
     }
 
@@ -39,6 +73,15 @@ object P3 {
      * the output should be sortByHeight(a) = [-1, 150, 160, 170, -1, -1, 180, 190].
      */
     fun sortByHeight(a: MutableList<Int>): MutableList<Int> {
+        val b = a.sorted().toMutableList().dropWhile { it == -1 }
+        var j = 0
+        for(i in 0 until a.size) {
+            if(a[i] != -1){
+                a[i] = b[j]
+                j++
+            }
+        }
+        return a
         TODO("not implemented")
     }
 
@@ -55,6 +98,32 @@ object P3 {
      * Because "foo(bar(baz))blim" becomes "foo(barzab)blim" and then "foobazrabblim".
      */
     fun reverseInParentheses(inputString: String): String {
+
+        val tmp = mutableListOf<Int>()
+        var tmpString = inputString
+        var returnString = ""
+        var index = 0
+
+        for(i in inputString.indices){
+            if(tmpString[i] == '('){
+                tmp.add(i)
+                index++
+            }
+            if(tmpString[i] == ')'){
+                tmpString = tmpString.replaceRange(
+                    tmp[index-1],
+                    i,
+                    tmpString.substring(tmp[index-1], i).reversed()
+                )
+                tmp.removeAt(index-1)
+                index--
+            }
+        }
+        for (i in tmpString.indices){
+            if(tmpString[i] != ')' && tmpString[i] != '(')
+                returnString += tmpString[i]
+        }
+        return returnString
         TODO("not implemented")
     }
 }
