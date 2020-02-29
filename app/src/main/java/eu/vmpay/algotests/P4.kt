ca710af2a4b41c6defa9737b10582548f12173d2
Itanus <rob.cwi@hotmail.com>
2020-02-29 21:01:06 +0100

    zadanie 4

--- app/src/main/java/eu/vmpay/algotests/P4.kt
+++ app/src/main/java/eu/vmpay/algotests/P4.kt
@@ -9,6 +9,11 @@ object P4 {
      * team 1, and the second element is the total weight of team 2 after the division is complete.
      */
     fun alternatingSums(a: MutableList<Int>): MutableList<Int> {
+        var first = 0
+        for (item in 0 until a.size step 2) {
+            first += a[item]
+        }
+        return mutableListOf(first, a.sum() - first)
         TODO("not implemented")
     }
 
@@ -16,6 +21,19 @@ object P4 {
      * Given a rectangular matrix of characters, add a border of asterisks(*) to it.
      */
     fun addBorder(picture: MutableList<String>): MutableList<String> {
+        var tmp: String
+        val newList = mutableListOf<String>()
+        var firstLine = ""
+        for (i in 0..picture[0].length + 1) {
+            firstLine += "*"
+        }
+        newList.add(0, firstLine)
+        for (item in picture) {
+            tmp = "*$item*"
+            newList.add(tmp)
+        }
+        newList.add(firstLine)
+        return newList
         TODO("not implemented")
     }
 
@@ -24,6 +42,14 @@ object P4 {
      * pair of elements in one of the arrays. Given two arrays a and b, check whether they are similar.
      */
     fun areSimilar(a: MutableList<Int>, b: MutableList<Int>): Boolean {
+        var counter = 0
+        if(a.sorted() != b.sorted())return false
+
+        for(i in 0..a.lastIndex){
+            if(a[i] != b[i])counter++
+            if(counter == 3)return false
+        }
+        return true
         TODO("not implemented")
     }
 
@@ -33,6 +59,14 @@ object P4 {
      * increasing sequence from the input.
      */
     fun arrayChange(inputArray: MutableList<Int>): Int {
+        var sum = 0
+        for(i in 0 until inputArray.lastIndex){
+            if(inputArray[i+1] <= inputArray[i] ) {
+                sum += inputArray[i] - inputArray[i+1] + 1
+                inputArray[i+1] = inputArray[i] + 1
+            }
+        }
+        return sum
         TODO("not implemented")
     }
 
@@ -40,6 +74,17 @@ object P4 {
      * Given a string, find out if its characters can be rearranged to form a palindrome.
      */
     fun palindromeRearranging(inputString: String): Boolean {
+        val newList = inputString.toSet()
+        var count = 0
+        for (item in newList) {
+            if (inputString.count { item == it } % 2 != 0 && inputString.length % 2 != 0) {
+                count++
+            }
+            if ((inputString.length % 2 != 0 && count > 1) || (inputString.count { item == it } % 2 != 0 && inputString.length % 2 == 0)) {
+                return false
+            }
+        }
+        return true
         TODO("not implemented")
     }
 
