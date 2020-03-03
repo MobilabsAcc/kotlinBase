c331c27a7af06ff898cf5fef78d4c383b79d7d51
Andrew <vmpay.eu@gmail.com>
2020-03-03 17:27:57 +0100

    P6 updated

--- app/src/main/java/eu/vmpay/algotests/P6.kt
+++ app/src/main/java/eu/vmpay/algotests/P6.kt
@@ -1,15 +1,41 @@
 package eu.vmpay.algotests
 
-/**
- * Challenge 2 - The cruise ship
- *
- * Four hundred people were traveling on a cruise ship with two engines having power of: 2kHP, 4kHP.
- * Fifty people represented the ship crew while others were tourists. The ship could have both engines
- * operating simultaneously, only one at a time or keep both off depending on the captain command.
- * Every tourist was assigned to a two or four person cabin shared with our tourists, which could be
- * his friends. The ship had a restaurant and a two bars of 300, 50 ,50 people capacity correspondingly.
- * Each tourist could visit one of the attractions or stay and his cabin at a time. Tourists under
- * 18 years were not allowed to enter the bar.
- *
- * Goal: implement the ship, engine, person, restaurant, bar, cabin classes reflecting the above case.
- */
+object P6 {
+
+    /**
+     * Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+     */
+    fun arrayReplace(inputArray: MutableList<Int>, elemToReplace: Int, substitutionElem: Int): MutableList<Int> {
+        TODO("not implemented")
+    }
+
+    /**
+     * Check if all digits of the given integer are even.
+     */
+    fun evenDigitsOnly(n: Int): Boolean {
+        TODO("not implemented")
+    }
+
+    /**
+     * Correct variable names consist only of English letters, digits and underscores and they can't
+     * start with a digit. Check if the given string is a correct variable name.
+     */
+    fun variableName(name: String): Boolean {
+        TODO("not implemented")
+    }
+
+    /**
+     * Given a string, your task is to replace each of its characters by the next one in the English alphabet;
+     * i.e. replace a with b, replace b with c, etc (z would be replaced by a).
+     */
+    fun alphabeticShift(inputString: String): String {
+        TODO("not implemented")
+    }
+
+    /**
+     * Given two cells on the standard chess board, determine whether they have the same color or not.
+     */
+    fun chessBoardCellColor(cell1: String, cell2: String) {
+        TODO("not implemented")
+    }
+}
