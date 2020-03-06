df5325fe4473c1683f584bffdd0187737867586c
jurgas <sebastianjurga7@gmail.com>
2020-03-06 17:24:23 +0100

    P6 done

--- app/src/main/java/eu/vmpay/algotests/P6.kt
+++ app/src/main/java/eu/vmpay/algotests/P6.kt
@@ -5,15 +5,28 @@ object P6 {
     /**
      * Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
      */
-    fun arrayReplace(inputArray: MutableList<Int>, elemToReplace: Int, substitutionElem: Int): MutableList<Int> {
-        TODO("not implemented")
+    fun arrayReplace(
+        inputArray: MutableList<Int>,
+        elemToReplace: Int,
+        substitutionElem: Int
+    ): MutableList<Int> {
+        for (i in inputArray.indices) {
+            if (inputArray[i] == elemToReplace)
+                inputArray[i] = substitutionElem
+        }
+        return inputArray
     }
 
     /**
      * Check if all digits of the given integer are even.
      */
     fun evenDigitsOnly(n: Int): Boolean {
-        TODO("not implemented")
+        val s = n.toString().toCharArray()
+        for (i in s.indices) {
+            if (s[i].toInt().rem(2) == 1)
+                return false
+        }
+        return true
     }
 
     /**
@@ -21,7 +34,14 @@ object P6 {
      * start with a digit. Check if the given string is a correct variable name.
      */
     fun variableName(name: String): Boolean {
-        TODO("not implemented")
+        val s = name.toCharArray()
+        if (s[0] in '1'..'9')
+            return false
+        for (i in 1..s.indices.last) {
+            if (s[i] !in 'a'..'z' && s[i] !in 'A'..'Z' && s[i] !in '0'..'9' && s[i] != '_')
+                return false
+        }
+        return true
     }
 
     /**
@@ -29,13 +49,34 @@ object P6 {
      * i.e. replace a with b, replace b with c, etc (z would be replaced by a).
      */
     fun alphabeticShift(inputString: String): String {
-        TODO("not implemented")
+        val s = inputString.toCharArray()
+        val builder = StringBuilder()
+        builder.append(s[s.indices.last])
+        for (i in 0 until s.indices.last)
+            builder.append(s[i])
+        return builder.toString()
     }
 
     /**
      * Given two cells on the standard chess board, determine whether they have the same color or not.
      */
-    fun chessBoardCellColor(cell1: String, cell2: String) {
-        TODO("not implemented")
+    fun chessBoardCellColor(cell1: String, cell2: String): Boolean {
+        if (cell1.length != 2 || cell2.length != 2)
+            return false
+        val c1Array = cell1.toCharArray()
+        val c2Array = cell2.toCharArray()
+        if (c1Array[0] !in 'A'..'Z' || c2Array[0] !in 'A'..'Z' || c1Array[1] !in '1'..'8' || c2Array[1] !in '1'..'8')
+            return false
+
+        var isWhiteCell1 = true
+        var isWhiteCell2 = true
+
+        if (((c1Array[0] - 'A').rem(2) == 1) == ((c1Array[1] - 1).toInt().rem(2) == 1))
+            isWhiteCell1 = false
+        if (((c2Array[0] - 'A').rem(2) == 1) == ((c2Array[1] - 1).toInt().rem(2) == 1))
+            isWhiteCell2 = false
+
+        return isWhiteCell1 == isWhiteCell2
+
     }
 }
--- app/src/test/java/eu/vmpay/algotests/P6Test.kt
+++ app/src/test/java/eu/vmpay/algotests/P6Test.kt
@@ -1,4 +1,58 @@
 package eu.vmpay.algotests
 
+import eu.vmpay.algotests.P6.alphabeticShift
+import eu.vmpay.algotests.P6.arrayReplace
+import eu.vmpay.algotests.P6.chessBoardCellColor
+import eu.vmpay.algotests.P6.evenDigitsOnly
+import eu.vmpay.algotests.P6.variableName
+import junit.framework.TestCase.*
+import org.junit.Test
+
 class P6Test {
+    @Test
+    fun arrayReplaceTest() {
+        assertEquals(
+            mutableListOf(1, 2, 3, 5, 5, 6, 7, 8),
+            arrayReplace(mutableListOf(1, 2, 3, 5, 5, 6, 7, 8), 4, 5)
+        )
+        assertEquals(mutableListOf(2, 2, 2, 2, 2), arrayReplace(mutableListOf(1, 1, 1, 1, 1), 1, 2))
+        assertEquals(mutableListOf(1, 2, 3), arrayReplace(mutableListOf(1, 2, 3), 4, 5))
+    }
+
+    @Test
+    fun evenDigitsOnlyTest() {
+        assertTrue(evenDigitsOnly(2))
+        assertTrue(evenDigitsOnly(24682))
+        assertFalse(evenDigitsOnly(1))
+        assertFalse(evenDigitsOnly(2468241))
+        assertFalse(evenDigitsOnly(12468240))
+    }
+
+    @Test
+    fun variableNameTest() {
+        assertTrue(variableName("afAae_14_"))
+        assertTrue(variableName("A"))
+        assertFalse(variableName("3"))
+        assertFalse(variableName("avmr_a1@"))
+        assertFalse(variableName("a]["))
+    }
+
+    @Test
+    fun alphabetShiftTest() {
+        assertEquals("xabcdefg", alphabeticShift("abcdefgx"))
+        assertEquals("x", alphabeticShift("x"))
+        assertEquals("xyz", alphabeticShift("yzx"))
+    }
+
+    @Test
+    fun chessBoardCellColorTest() {
+        assertTrue(chessBoardCellColor("B2", "G5"))
+        assertTrue(chessBoardCellColor("C7", "H8"))
+        assertTrue(chessBoardCellColor("B8", "E5"))
+        assertFalse(chessBoardCellColor("B8h", "E5"))
+        assertFalse(chessBoardCellColor("b8", "E5"))
+        assertFalse(chessBoardCellColor("B8", "E9"))
+        assertFalse(chessBoardCellColor("A1", "A2"))
+        assertFalse(chessBoardCellColor("B1", "H8"))
+    }
 }
